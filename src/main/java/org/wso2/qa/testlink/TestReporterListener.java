b618cabb08fc296862e12794ada4c8740e7a65bf
nu1silva <nu1silva@gmail.com>
2017-09-12 18:03:28 +0530

    Fixing copyrights

--- src/main/java/org/wso2/qa/testlink/TestReporterListener.java
+++ src/main/java/org/wso2/qa/testlink/TestReporterListener.java
@@ -1,13 +1,13 @@
-/**
+/*
  * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * <p>
+ *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -40,13 +40,11 @@ import java.util.Map;
  */
 public class TestReporterListener implements IReporter {
 
+    private final Logger logger = LoggerFactory.getLogger(TestReporterListener.class);
     private Connector connector = new Connector();
     private Util util = new Util();
-
     private int buildNumber = 0;
     private String platform = null;
-
-    private final Logger logger = LoggerFactory.getLogger(TestReporterListener.class);
 
     /**
      * Using the listener iterates through each suite to get the results and update
